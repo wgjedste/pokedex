f6146754730751cc7342b3241099e8068059b64a
Will Gjedsted <wgjedste@gmail.com>
2021-01-02 13:37:06 -0800

    Added Pokemon

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
-alert('Hello world');
+pokemons= [
+    { name: "Bulbasaur", height: 0.7, types: ["grass"] },
+    { name: "Charmander", height: 0.6, types: ["fire"] },
+    { name: "Golbat", height: 1.6, types: ["flying", "poison"] },
+    { name: "Alakazam", height: 1.5, types: ["psychic"] },
+  ]
 
-let favoriteFood  = 'Pasta';
-document.write(favoriteFood);
+  console.log(pokemons)
