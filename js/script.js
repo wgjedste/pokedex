3f35f661804961d7e3f588ae30d17da2c936fa23
Will Gjedsted <wgjedste@gmail.com>
2021-01-03 17:38:03 -0800

    Added pokemonList

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
-pokemons= [
+let pokemonList = [
     { name: "Bulbasaur", height: 0.7, types: ["grass"] },
     { name: "Charmander", height: 0.6, types: ["fire"] },
     { name: "Golbat", height: 1.6, types: ["flying", "poison"] },
     { name: "Alakazam", height: 1.5, types: ["psychic"] },
+    { name: "Hypno", height: 1.6, types: ["psychic"]},
   ]
 
-  console.log(pokemons)
+  console.log(pokemonList); 
