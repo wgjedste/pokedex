f60265f3de8343c2687170b877dd222c1c04ca02
Will Gjedsted <wgjedste@gmail.com>
2021-10-15 07:29:55 -0700

    test-1

--- js/script.js
+++ js/script.js
@@ -12,33 +12,61 @@ let pokemonRepository = (function () {
   }
 
   // ADD LIST ITEM - Convert to JQUERY
+  // function addListItem(pokemon) {
+  //   pokemonRepository.loadDetails(pokemon).then(function () {
+  //     // let ul = $('.pokemon-list')
+  //     // let li = $('<li></li>')
+  //     var $row = $(".row");
+
+  //     var $card = $('<div class="card" style="width:400px"></div>');
+  //     var $image = $(
+  //       '<img class="card-img-top mx-auto" alt="Card image" style="width:20%" />'
+  //     );
+  //     $image.attr("src", pokemon.imageUrl);
+  //     var $cardBody = $('<div class="card-body"></div>');
+  //     var $cardTitle = $("<h4 class='card-title' >" + pokemon.name + "</h4>");
+  //     let $seeProfile = $(
+  //       '<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">' +
+  //         pokemon.name +
+  //         "</button>"
+  //     );
+  //     // li.append(button)
+  //     // ul.append(li)
+  //     $row.append($card);
+  //     //Append the image to each card
+  //     $card.append($image);
+  //     $card.append($cardBody);
+  //     $cardBody.append($cardTitle);
+  //     $cardBody.append($seeProfile);
+  //     $seeProfile.on("click", function (event) {
+  //       showDetails(pokemon);
+  //     });
+  //   });
+  // }
+
   function addListItem(pokemon) {
     pokemonRepository.loadDetails(pokemon).then(function () {
-      // let ul = $('.pokemon-list')
-      // let li = $('<li></li>')
-      var $row = $(".row");
+      let row = $(".row");
 
-      var $card = $('<div class="card" style="width:400px"></div>');
-      var $image = $(
-        '<img class="card-img-top mx-auto" alt="Card image" style="width:20%" />'
+      let card = $(
+        '<div class="card mt-5" style="width: 15rem; margin:13px;"></div>'
       );
-      $image.attr("src", pokemon.imageUrl);
-      var $cardBody = $('<div class="card-body"></div>');
-      var $cardTitle = $("<h4 class='card-title' >" + pokemon.name + "</h4>");
-      let $seeProfile = $(
-        '<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">' +
-          pokemon.name +
-          "</button>"
+      let image = $('<img class="card-img-top" alt="...">');
+      let title = $('<h5 class="card-title">' + pokemon.name + "</h5>");
+      image.attr("src", pokemon.imageUrl);
+      let body = $('<div class="card-body" style="text-align: center;"></div>');
+      let button = $(
+        '<button type="button" class="btn" style="background-color: #d88780; color: white" data-toggle="modal" data-target="#exampleModal">See profile</button>'
       );
-      // li.append(button)
-      // ul.append(li)
-      $row.append($card);
-      //Append the image to each card
-      $card.append($image);
-      $card.append($cardBody);
-      $cardBody.append($cardTitle);
-      $cardBody.append($seeProfile);
-      $seeProfile.on("click", function (event) {
+
+      //append
+      row.append(card);
+      card.append(image);
+      card.append(body);
+      body.append(title);
+      body.append(button);
+
+      button.on("click", function (event) {
         showDetails(pokemon);
       });
     });
@@ -83,6 +111,32 @@ let pokemonRepository = (function () {
 
     let pokemonSpecies = $("<p>" + "species: " + pokemon.species + "</p>");
     console.log(modalTitle);
+
+    if (pokemon.types.includes("grass")) {
+      $(".modal-header").css("background-color", "rgb(120, 200, 80)");
+    } else if (pokemon.types.includes("fire")) {
+      $(".modal-header").css("background-color", "rgb(240, 128, 48)");
+    } else if (pokemon.types.includes("poison")) {
+      $(".modal-header").css("background-color", "rgb(168, 144, 240)");
+    } else if (pokemon.types.includes("water")) {
+      $(".modal-header").css("background-color", "rgb(104, 144, 240)");
+    } else if (pokemon.types.includes("bug")) {
+      $(".modal-header").css("background-color", "rgb(168, 184, 32)");
+    } else if (pokemon.types.includes("water")) {
+      $(".modal-header").css("background-color", "rgb(69, 120, 237)");
+    } else if (pokemon.types.includes("ice")) {
+      $(".modal-header").css("background-color", "rgb(66, 174, 174)");
+    } else if (pokemon.types.includes("electric")) {
+      $(".modal-header").css("background-color", "rgb(252, 234, 161)");
+    } else if (pokemon.types.includes("ground")) {
+      $(".modal-header").css("background-color", "rgb(219, 181, 77)");
+    } else if (pokemon.types.includes("fairy")) {
+      $(".modal-header").css("background-color", "rgb(232, 120, 144)");
+    } else if (pokemon.types.includes("ghost")) {
+      $(".modal-header").css("background-color", "rgb(100, 78, 136)");
+    } else if (pokemon.types.includes("normal")) {
+      $(".modal-header").css("background-color", "rgb(156, 156, 99)");
+    }
 
     modalTitle.append(pokemonName);
     // modalBody.append(pokemonImage);
@@ -171,3 +225,26 @@ pokemonRepository.loadList().then(function () {
     pokemonRepository.addListItem(pokemon);
   });
 });
+
+
+// FUNCTION SEARCH
+function search() {
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("myUL");
+  // li = ul.getElementsByTagName("");
+  li = ul.querySelectorAll(".card");
+  // console.log(li[0].querySelector(".card-body").querySelector(".card-title"));
+  for (i = 0; i < li.length; i++) {
+    // a = li[i].getElementsByTagName("a")[0];
+    a = li[i].querySelector(".card-body").querySelector(".card-title");
+    console.log(a.innerText);
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
